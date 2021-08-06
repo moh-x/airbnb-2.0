b4a7228d4f55cb2711db490c4879e4129ff301d5
Babatunde Adenowo <mo.xodeeq@gmail.com>
2021-08-06 15:06:09 +0100

    Search page built

--- components/Header.js
+++ components/Header.js
@@ -6,13 +6,48 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 
-function Header() {
+function Header({ placeholder }) {
+  const router = useRouter();
+  const [searchInput, setSearchInput] = useState("");
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRange = { startDate, endDate, key: "selection" };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white dark:bg-gray-900 shadow-md p-5 md:px-10 ">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
-          src="https://links.papareact.com/qd3"
+          src="https://res.cloudinary.com/moh-x/image/upload/v1628243962/projects/airbnb/Airbnb_Logo_B%C3%A9lo.svg_be1egb.png"
           alt=""
           layout="fill"
           objectFit="contain"
@@ -22,9 +57,11 @@ function Header() {
 
       <div className="flex items-center md:border-2 md:dark:border-gray-200 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 dark:text-gray-200 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 dark:bg-gray-100 text-white dark:text-red-600 rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -38,6 +75,44 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+
+          <div className="flex">
+            <button
+              onClick={() => setSearchInput("")}
+              className="flex-grow text-gray-500"
+            >
+              Cancel
+            </button>
+            <button onClick={search} className="flex-grow text-red-400">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
---
+++ pages/search.js
@@ -0,0 +1,67 @@
+import { format } from "date-fns";
+import { useRouter } from "next/dist/client/router";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
+
+function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+  const formatDate = (dateString) =>
+    format(new Date(dateString), "dd MMMM yyyy");
+  const range = `${formatDate(startDate)} - ${formatDate(endDate)}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
+
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <button className="button">Cancellation flexibility</button>
+            <button className="button">Type of Place</button>
+            <button className="button">Price</button>
+            <button className="button">Rooms and Beds</button>
+            <button className="button">More filters</button>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://jsonkeeper.com/b/5NPS").then(
+    (res) => res.json()
+  );
+
+  return { props: { searchResults } };
+}
